fbd49c5d30f15bc2acdbd205f7b7362cea37c725
Winterstorm <thunpwnz@gmail.com>
2018-07-13 23:22:21 +0400

    zoom state added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,44 @@ import React from 'react';
 import './App.css';
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            zoom: 13,
+            maptype: 'roadmap',
+        }
+    }
     componentDidMount() {
         let map;
         map = new window.google.maps.Map(document.getElementById('map'), {
             center: {lat: -33.8688, lng: 151.2195},
-            zoom: 13,
+            zoom: this.state.zoom,
             mapTypeId: 'roadmap',
+        });
+
+        map.addListener('zoom_changed', () => {
+            this.setState({
+                zoom: map.getZoom(),
+            });
+        });
+
+        map.addListener('maptypeid_changed', () => {
+            this.setState({
+                maptype: map.getMapTypeId(),
+            });
         });
     }
 
     render() {
         return (
             <div id='app'>
+                <div id='state'>
+                    <h1>State</h1>
+                    <p>
+                        Zoom level: {this.state.zoom}<br />
+                        Map type: {this.state.maptype}
+                    </p>
+                </div>
                 <div id='map' />
             </div>
         );
