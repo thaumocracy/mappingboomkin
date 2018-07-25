54df7207da503e37a5d8a9782b2c82651bafa47a
Winterstorm <thunpwnz@gmail.com>
2018-07-25 18:03:10 +0400

    Broken commit

--- src/App.js
+++ src/App.js
@@ -1,95 +1,175 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            zoom: 8,
+class App extends Component {
+        state = {
+            map : {},
+            zoom: 12,
             maptype: 'roadmap',
+            openWindow : false,
+            markers : [],
+            currentMarker : null,
             locations : [
                 {
                     'name': "Mari Vanna",
                     'type': "breakfast",
                     'location' : {lat:51.501816 , lng : -0.162474 },
-                    'streetAddress': "116 Knightsbridge London SW1X 7PJ United Kingdom"
+                    'streetAddress': "116 Knightsbridge London SW1X 7PJ United Kingdom",
+                    "id" : "4f2c0acae4b048e466df240b",
                 },
                 {
                     'name': "Bob Bob Ricard",
                     'type': "bar",
                     'location' : {lat:51.512311 , lng : -0.137207 },
-                    'streetAddress': "1 Upper James Street Soho W1F 9DF United Kingdom"
+                    'streetAddress': "1 Upper James Street Soho W1F 9DF United Kingdom",
+                    "id" : "4ad73a36f964a5200d0921e3",
                 },
                 {
                     'name': "Mamuśka!",
                     'type': "dinner",
                     'location' : {lat:51.492953 , lng : -0.100232 },
-                    'streetAddress': "16 Elephant and Castle London SE1 6TH United Kingdom"
+                    'streetAddress': "16 Elephant and Castle London SE1 6TH United Kingdom",
+                    "id" : "4b745da9f964a520f2d72de3",
                 },
                 {
                     'name': "Borshtch ‘N Tears",
                     'type': "dinner",
                     'location' : {lat:51.497554 , lng : -0.164939 },
-                    'streetAddress': "46 Beauchamp Place London SW3 1NX United Kingdom"
+                    'streetAddress': "46 Beauchamp Place London SW3 1NX United Kingdom",
+                    "id" : "4c3f4c4dda3dc928eeacc5b9",
                 },
                 {
                     'name': "Vatrushka Russian Bakery & Café",
                     'type': "cafe",
                     'location' : {lat:51.466437 , lng : -0.297305 },
-                    'streetAddress': "112 Kew Road Richmond London TW9 2PQ United Kingdom"
+                    'streetAddress': "112 Kew Road Richmond London TW9 2PQ United Kingdom",
+                    "id" : "4c276312f1272d7fd30e88c5",
                 },
                 {
                     'name': "Rasputin",
                     'type': "bar",
                     'location' : {lat:51.508215 , lng : -0.275221 },
-                    'streetAddress': "265 High St London W3 9BY United Kingdom"
+                    'streetAddress': "265 High St London W3 9BY United Kingdom",
+                    "id" : "527c07e711d255a8e45a3856",
                 },
                 {
                     'name': "ZIMA",
                     'type': "bar",
                     'location' : {lat:51.513356 , lng : -0.131518 },
-                    'streetAddress': "45 Frith Street Soho London W1D 4SD United Kingdom"
+                    'streetAddress': "45 Frith Street Soho London W1D 4SD United Kingdom",
+                    "id" : "4bf565aaff90c9b6064f5628",
                 },
                 {
                     'name': "Assorti",
                     'type': "dinner",
                     'location' : {lat:51.500825 , lng : -0.013597 },
-                    'streetAddress': "Unit 1 115 Meridian Place Marsh Wall London E14 9FE United Kingdom"
+                    'streetAddress': "Unit 1 115 Meridian Place Marsh Wall London E14 9FE United Kingdom",
+                    "id" : "4b851c53f964a520684c31e3",
                 },
                 {
                     'name': "The Merchant’s Yard",
                     'type': "bakery",
                     'location' : {lat:51.497431 , lng : -0.164639 },
-                    'streetAddress': "Unit 1 115 Meridian Place Marsh Wall London E14 9FE United Kingdom"
+                    'streetAddress': "Unit 1 115 Meridian Place Marsh Wall London E14 9FE United Kingdom",
+                    "id" : "4ac518e0f964a52015aa20e3",
                 }
             ]
         }
+    getMarkerInfo (marker) {
+        let clientID = "AZREHK4CD0M2LQ0S0WDTBURVJL3USHZFFXK4EJYBNA3BUZ42";
+        let clientSecret = "CFEODDWIQNMOOUSLPIU4IAIRP2O0KIKIEXPTRA21345BI1MC";
+        let url = "https://api.foursquare.com/v2/venues/search?client_id=" + clientID + "&client_secret=" + clientSecret + "&v=20130815&ll=" + marker.position.lat() + "," + marker.position.lng() + "&limit=1";
+        console.log(marker.position.lat());
+        fetch(url).then(data => data.json()).then(item => console.log(item.response.venues[0].id));
+
+
+    }
+
+    getTips (marker) {
+        let clientID = "AZREHK4CD0M2LQ0S0WDTBURVJL3USHZFFXK4EJYBNA3BUZ42";
+        let clientSecret = "CFEODDWIQNMOOUSLPIU4IAIRP2O0KIKIEXPTRA21345BI1MC";
+        let url = "https://api.foursquare.com/v2/venues/4f2c0acae4b048e466df240b/tips?client_id=" + clientID + "&client_secret=" + clientSecret + "&ll=" + "51.501816" + "," + "-0.162474" + "&limit=1";
+
+        fetch(url).then(data => data.json()).then(item => console.log(item));
+
+
     }
     makeMarkers (locations , map) {
+        let markers = [];
+        let joystick = this;
         for(let i = 0;i < locations.length;i++){
             let position = locations[i].location;
             let title = locations[i].name;
 
+            // let infowindow = new window.google.maps.InfoWindow({
+            //     content: title
+            // });
             let marker = new window.google.maps.Marker({
-                map:map,
-                position:position,
-                title:title,
-                animation: window.google.maps.Animation.DROP,
-                id:i
-            })
+                map : map,
+                position : position,
+                title : title,
+                animation : window.google.maps.Animation.DROP,
+                id : i,
+            });
+
+            marker.addListener('click', function() {
+                joystick.openWindow(marker)
+                joystick.getMarkerInfo(marker);
+            });
+            marker.addListener('closeclick', function() {
+                joystick.closeWindow(marker);
+            });
+            marker.addListener('click', toggleBounce);
+
+            function toggleBounce() {
+                if (marker.getAnimation() !== null) {
+                    marker.setAnimation(null);
+                } else {
+                    marker.setAnimation(window.google.maps.Animation.BOUNCE);
+                }
+            }
+            markers.push(marker);
         }
-    }
+        this.setState({markers:markers});
+    };
+    testFunction = () => {
+        let { markers } = this.state.markers;
+        console.log(markers);
+        };
+
+
+    openWindow = (marker) => {
+            // let marker = this.state.markers.findIndex(event.target. =>)
+        this.setState({
+            openWindow: true,
+            currentMarker: marker,
+        });
+        console.log(marker);
+        this.getMarkerInfo(marker)
+    };
+    closeWindow = (marker) => {
+        this.setState({
+            openWindow: false,
+            currentMarker: null
+        });
+        console.log(marker);
+    };
+
+    clearMarkers = () => {
+        debugger;
+        const { locations } = this.state.locations;
+        console.log(locations);
+    };
+
     componentDidMount() {
-        let { locations } = this.state;
-        let map;
-        let markers = [];
+        let { locations , map } = this.state;
         map = new window.google.maps.Map(document.getElementById('map'), {
             center: {lat: 51.507351, lng: -0.127758},
             zoom: this.state.zoom,
             mapTypeId: 'roadmap',
         });
-        this.makeMarkers(locations, map);
+        this.setState({map});
 
         map.addListener('zoom_changed', () => {
             this.setState({
@@ -102,22 +182,34 @@ class App extends React.Component {
                 maptype: map.getMapTypeId(),
             });
         });
-    }
 
+        map.addListener('click', () => {
+            this.setState({
+                currentMarker : null,
+            });
+        });
+        this.makeMarkers(locations, map);
+        // this.getTips();
+        this.testFunction()
+    }
     render() {
-        const { locations } = this.state;
+        const { locations  } = this.state;
         return (
             <div id='app'>
                 <div id='state'>
-                    <h1>State</h1>
+                    <h1 onClick={() => this.clearMarkers()}>State</h1>
                     <p>
                         Zoom level: {this.state.zoom}<br />
-                        Map type: {this.state.maptype}
+                        Map type: {this.state.maptype}<br />
+                        Marker : {this.state.currentMarker ? this.state.currentMarker.title : "Can't load the marker"}
                     </p>
                     <div>
                         {locations.map( (place) => {
                             return (
-                                <h2 key={place.name}>{place.name}</h2>
+                                <h2
+                                    key={place.name}
+                                    onClick={(marker) => this.openWindow(marker)}
+                                >{place.name}</h2>
                             )
                         })}
                     </div>
